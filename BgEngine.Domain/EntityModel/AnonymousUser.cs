593fda58bb04f17317fe51f696c1f810cc028d72
Yago Perez Vazquez <yawette@hotmail.com>
2012-06-18 18:38:28 +0200

    initial load

---
+++ BgEngine.Domain/EntityModel/AnonymousUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace BgEngine.Domain.EntityModel
+{
+    public class AnonymousUser
+    {
+        public AnonymousUser() { }
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="username">The user name</param>
+        /// <param name="email">Email for the user</param>
+        /// <param name="web">Web if have any</param>
+        public AnonymousUser(string username, string email, string web)
+        {
+            this.Username = username;
+            this.Email = email;
+            this.Web = web;
+        }
+        /// <summary>
+        /// The AnonymousUser name
+        /// </summary>
+        [Display(ResourceType = typeof(Resources.AppMessages), Name = "AnonymousUser_Username", Prompt = "AnonymousUser_Username_Prompt")]
+        public string Username { get; set; }
+        /// <summary>
+        /// The AnonymousUser Email
+        /// </summary>
+        [Display(ResourceType = typeof(Resources.AppMessages), Name = "AnonymousUser_Email", Prompt = "AnonymousUser_Email_Prompt")]
+        public string Email { get; set; }
+        /// <summary>
+        /// The AnonymousUser Web
+        /// </summary>
+        [Display(ResourceType = typeof(Resources.AppMessages), Name = "AnonymousUser_Web", Prompt = "AnonymousUser_Web_Prompt")]
+        public string Web { get; set; }
+
+        /// <summary>
+        /// Complex type check for null values because Complex types are always required
+        /// </summary>
+        public bool HasValue
+        {
+            get
+            {
+                return (Username != null || Email != null || Web != null);
+            }
+        }
+    }
+}
---
+++ BgEngine.Infraestructure/Validation/EntityValidator.cs
@@ -0,0 +1,116 @@
+﻿//==============================================================================
+// This file is part of BgEngine.
+//
+// BgEngine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BgEngine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with BgEngine. If not, see <http://www.gnu.org/licenses/>.
+//==============================================================================
+// Copyright (c) 2011 Yago Pérez Vázquez
+// Version: 1.0
+//==============================================================================
+
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+using BgEngine.Domain.Validation;
+
+namespace BgEngine.Infraestructure.Validation
+{
+
+    /// <summary>
+    /// Validator based on Data Annotations. 
+    /// This validator use IValidatableObject interface and
+    /// ValidationAttribute ( hierachy of this) for
+    /// perform validation
+    /// </summary>
+    public class EntityValidator : IEntityValidator
+    {
+        /// <summary>
+        /// Get errors if object implement IValidatableObject
+        /// </summary>
+        /// <typeparam name="TEntity">The typeof entity</typeparam>
+        /// <param name="item">The item to validate</param>
+        /// <param name="errors">A collection of current errors</param>
+        void SetValidatableObjectErrors<TEntity>(TEntity item, List<string> errors) where TEntity : class
+        {
+            if (typeof(IValidatableObject).IsAssignableFrom(typeof(TEntity)))
+            {
+                var validationContext = new ValidationContext(item, null, null);
+
+                var validationResults = ((IValidatableObject)item).Validate(validationContext);
+
+                errors.AddRange(validationResults.Select(vr => vr.ErrorMessage));
+            }
+        }
+
+        /// <summary>
+        /// Get errors on ValidationAttribute
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity</typeparam>
+        /// <param name="item">The entity to validate</param>
+        /// <param name="errors">A collection of current errors</param>
+        void SetValidationAttributeErrors<TEntity>(TEntity item, List<string> errors) where TEntity : class
+        {
+            var result = from property in TypeDescriptor.GetProperties(item).Cast<PropertyDescriptor>()
+                         from attribute in property.Attributes.OfType<ValidationAttribute>()
+                         where !attribute.IsValid(property.GetValue(item))
+                         select attribute.FormatErrorMessage(string.Empty);
+
+            if (result != null
+                &&
+                result.Any())
+            {
+                errors.AddRange(result);
+            }
+        }
+        /// <summary>
+        /// Check if the Entity is valid
+        /// </summary>
+        /// <typeparam name="TEntity">The Entity type</typeparam>
+        /// <param name="item">The Entity</param>
+        /// <returns>true of valid</returns>
+        public bool IsValid<TEntity>(TEntity item) where TEntity : class
+        {
+
+            if (item == null)
+                return false;
+
+            List<string> validationErrors = new List<string>();
+
+            SetValidatableObjectErrors(item, validationErrors);
+            SetValidationAttributeErrors(item, validationErrors);
+
+            return !validationErrors.Any();
+        }
+        /// <summary>
+        /// Get the validdation messages
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <param name="item">The Entity</param>
+        /// <returns>List of messages</returns>
+        public IEnumerable<string> GetInvalidMessages<TEntity>(TEntity item) where TEntity : class
+        {
+            if (item == null)
+                return null;
+
+            List<string> validationErrors = new List<string>();
+
+            SetValidatableObjectErrors(item, validationErrors);
+            SetValidationAttributeErrors(item, validationErrors);
+
+
+            return validationErrors;
+        }
+    }
+}
