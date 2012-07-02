a75f0e47b7109bc5d3a26fa2d58ec9a492aad4dd
Yago Perez Vazquez <yawette@hotmail.com>
2012-07-02 23:55:24 +0200

    Changed field lengths

--- BgEngine.Domain/EntityModel/Comment.cs
+++ BgEngine.Domain/EntityModel/Comment.cs
@@ -79,9 +79,11 @@ namespace BgEngine.Domain.EntityModel
         public bool IsSpam { get; set; }
         
         [Display(ResourceType = typeof(Resources.AppMessages), Name = "Comment_Ip", Prompt = "Comment_Ip_Prompt")]
+        [StringLength(100, ErrorMessageResourceType = typeof(Resources.AppMessages), ErrorMessageResourceName = "ErrorLenght")]
         public string Ip { get; set; }
         
         [Display(ResourceType = typeof(Resources.AppMessages), Name = "Comment_UserAgent", Prompt = "Comment_UserAgent_Prompt")]
+        [StringLength(200, ErrorMessageResourceType = typeof(Resources.AppMessages), ErrorMessageResourceName = "ErrorLenght")]
         public string UserAgent { get; set; }
 
         /// <summary>
